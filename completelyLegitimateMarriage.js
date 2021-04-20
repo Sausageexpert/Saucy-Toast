c18fb0141e6b65c5c89863856806ae1f77790c1a
Sausageexpert <68068569+Sausageexpert@users.noreply.github.com>
2021-04-20 15:01:41 +0530

    Add files via upload

---
+++ completelyLegitimateMarriage.js
@@ -0,0 +1,23 @@
+/*
+Welcome, one and all, to this completely legitimate marriage. I have absolutely no idea what I'm
+supposed to be doing, but we can start by throwing rice around, since that seems like a good idea.
+
+After that we have virtual refreshments served in the virtualJail file, so you can hop over there
+
+I'm also not sure if Ms. Squiggly Underline is married, so if she is... that might be a teeny-weeny
+bit of a problem, but no one really cares, since everyone likes weddings (for the food mostly, but still)
+
+So if Ms. Squiggly Underline is married (poor husband), then we might have to cancel this programme.
+Or we could just go ahead and marry off her sister, who is also very conveniently named Ms. Squiggly
+Underline (they were twins, and their parents couldn't think of another name).
+
+So I think I'll write some code now
+
+if(Mrs.SquigglyUnderline.isMarried()){
+    alert("Oops");
+} else {
+    alert("Yay! Let's go ahead with the wedding then!")
+}
+
+That's a great function! Anyway, time for the best part! REFRESHMENTS!
+*/
